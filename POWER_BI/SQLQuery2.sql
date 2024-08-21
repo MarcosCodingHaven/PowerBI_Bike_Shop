f5a50b95d54bfe3bcb143846018219a6bac859bb
Marco <marco.behrens1999@gmail.com>
2024-08-21 14:17:44 +0200

    Add files via upload
    Uploading the full project

---
+++ POWER_BI/SQLQuery2.sql
@@ -0,0 +1,22 @@
+with cte as (
+select * from bike_share_yr_0
+union
+select * from bike_share_yr_1)
+
+
+select 
+dteday,
+season,
+a.yr,
+weekday,
+hr,
+rider_type,
+riders,
+price / 100 AS price,
+COGS / 100 AS COGS,
+riders * price / 100 as revenue,
+(riders * price -(COGS * riders))/ 100 as profit
+from cte a
+left join cost_table b 
+on a.yr = b.yr
+
